a37b4f647aa672b5ec1c32329001830c50af7cd3
MohammadSajjadHossain9648 <sajjadhossain9648@gmail.com>
2022-02-07 21:03:02 +0600

    final commit

---
+++ Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Setter_getter
+{
+    internal class Movie
+    {
+        public string movieName;
+        public string creator;
+        public string rating;
+
+        public Movie(string mN, string c, string r)
+        {
+            movieName = mN;
+            creator = c;
+            Rating = r;
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+            set {
+                if(value == "PG" || value == "PG-13" || value == "G" || value == "R")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Setter_getter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Tom & Jerry","William","PG-13");
+            Movie movie2 = new Movie("The adventure of Tin Tin","Herge","R");
+
+            movie1.Rating = "cat"; //always remember the next line about rating. 
+            Console.WriteLine("Valid Rating are PG, PG-13, R, G and non-valid rating is NR.");
+            Console.WriteLine(movie1.Rating);
+            Console.ReadLine();
+        }
+    }
+}
